0340c8ac9fa995f3a5c94f3c2779302c614e3be8
kangwongu <kwg0527@naver.com>
2022-01-12 11:04:57 +0900

    week04 projects

---
+++ python/sparta/pjojects/bookreview/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, jsonify, request
+app = Flask(__name__)
+
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
+
+## HTML을 주는 부분
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+## API 역할을 하는 부분
+@app.route('/review', methods=['POST'])
+def write_review():
+    # 제목, 저자, 리뷰를 가져옮
+    title_receive = request.form['title_give']
+    author_receive = request.form['author_give']
+    review_receive = request.form['review_give']
+
+    # json으로 만들고
+    doc = {
+        'title':title_receive,
+        'author':author_receive,
+        'review':review_receive
+    }
+
+    # db에 저장
+    db.bookreview.insert_one(doc)
+
+    return jsonify({'msg': '저장 완료!'})
+
+
+@app.route('/review', methods=['GET'])
+def read_reviews():
+    reviews = list(db.bookreview.find({}, {'_id': False}))
+
+    return jsonify({'all_reviews': reviews})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
