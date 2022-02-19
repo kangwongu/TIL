45a1a03570ab21709f0bcd3895aa6b0f087c5389
kangwongu <kwg0527@naver.com>
2022-02-19 22:44:49 +0900

    week04

--- Spring/sparta/Spring_advanced/springcore/src/main/java/com/sparta/springcore/service/ProductService.java
+++ Spring/sparta/Spring_advanced/springcore/src/main/java/com/sparta/springcore/service/ProductService.java
@@ -5,15 +5,17 @@ import com.sparta.springcore.dto.ProductRequestDto;
 import com.sparta.springcore.model.Product;
 import com.sparta.springcore.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class ProductService {
 
     private final ProductRepository productRepository;
-    public static final int MIN_MY_PRICE = 100;    // 최저 가격 설정의 최소값 ( 이 값보다 작으면 예외 )
+    public static final int MIN_MY_PRICE = 100;
 
     @Autowired
     public ProductService(ProductRepository productRepository) {
@@ -21,7 +23,7 @@ public class ProductService {
     }
 
     public Product createProduct(ProductRequestDto requestDto, Long userId ) {
-        // 요청받은 DTO 로 DB에 저장할 객체 만들기
+    // 요청받은 DTO 로 DB에 저장할 객체 만들기
         Product product = new Product(requestDto, userId);
 
         productRepository.save(product);
@@ -31,8 +33,8 @@ public class ProductService {
 
     public Product updateProduct(Long id, ProductMypriceRequestDto requestDto) {
         int myprice = requestDto.getMyprice();
-        if(myprice < MIN_MY_PRICE) {
-            throw new IllegalArgumentException("유효하지 않은 관심 가격입니다. 최소 " + MIN_MY_PRICE + "원 이상으로 설정해 주세요.");
+        if (myprice < MIN_MY_PRICE) {
+            throw new IllegalArgumentException("유효하지 않은 관심 가격입니다. 최소 " + MIN_MY_PRICE + " 원 이상으로 설정해 주세요.");
         }
 
         Product product = productRepository.findById(id)
@@ -45,12 +47,20 @@ public class ProductService {
     }
 
     // 회원 ID 로 등록된 상품 조회
-    public List<Product> getProducts(Long userId) {
-        return productRepository.findAllByUserId(userId);
+    public Page<Product> getProducts(Long userId, int page, int size, String sortBy, boolean isAsc) {
+        Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
+        Sort sort = Sort.by(direction, sortBy);
+        Pageable pageable = PageRequest.of(page, size, sort);
+
+        return productRepository.findAllByUserId(userId, pageable);
     }
 
-    // 관리자용, 등록된 모든 관심 상품 조회
-    public List<Product> getAllProducts() {
-        return productRepository.findAll();
+    // (관리자용) 상품 전체 조회
+    public Page<Product> getAllProducts(int page, int size, String sortBy, boolean isAsc) {
+        Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
+        Sort sort = Sort.by(direction, sortBy);
+        Pageable pageable = PageRequest.of(page, size, sort);
+
+        return productRepository.findAll(pageable);
     }
 }
