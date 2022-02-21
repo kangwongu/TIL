35f7b017219c2e277137c63cbb4b4e13d10bc230
kangwongu <kwg0527@naver.com>
2022-02-21 11:56:30 +0900

    linkedlist

--- DataStructure/linkedlist/DoubleLinkedList.java
+++ DataStructure/linkedlist/DoubleLinkedList.java
@@ -1,26 +1,28 @@
 package linkedlist;
 
 public class DoubleLinkedList<T> {
-    public Node<T> head = null;
-    public Node<T> tail = null;
+    
+    private Node<T> head = null;
+    private Node<T> tail = null;
     
     public class Node<T> {
         T data;
         Node<T> prev = null;
         Node<T> next = null;
         
-        public Node(T data) {
+        Node(T data) {
             this.data = data;
         }
     }
     
-    // 기능
     // 데이터 추가
     public void addNode(T data) {
+        // 1. 리스트에 노드가 하나도 없는지
         if(head == null) {
             head = new Node<T>(data);
             tail = head;
         } else {
+        // 2. 리스트에 노드가 하나 이상 있는지
             Node<T> node = head;
             while(node.next != null) {
                 node = node.next;
@@ -33,9 +35,11 @@ public class DoubleLinkedList<T> {
     
     // 데이터 출력
     public void printAll() {
+        // 1. 리스트에 노드가 하나도 없는지
         if(head == null) {
-            System.out.println("리스트에 데이터가 없어요");
+            System.out.println("리스트에 노드가 없어요");
         } else {
+            // 2. 리스트에 노드가 하나 이상 있는지
             Node<T> node = head;
             while(node != null) {
                 System.out.println(node.data);
@@ -44,27 +48,12 @@ public class DoubleLinkedList<T> {
         }
     }
     
-    // head부터 특정 노드 찾기
-    public T searchFromHead(T data) {
-        if(head == null) {
-            return null;
-        } else {
-            Node<T> node = head;
-            while(node != null) {
-                if(node.data == data) {
-                    return node.data;
-                } else {
-                    node = node.next;
-                }
-            }
-            return null;
-        }
-    }
-    
     // tail부터 특정 노드 찾기
     public T searchFromTail(T data) {
+        // 1. 리스트에 노드가 하나도 없는지
         if(head == null) {
             return null;
+        // 2. 리스트에 노드가 하나 이상 있는지
         } else {
             Node<T> node = tail;
             while(node != null) {
@@ -78,63 +67,86 @@ public class DoubleLinkedList<T> {
         }
     }
     
-    // 데이터를 임의 노드 앞에 노드를 추가
-    public boolean insertToFront(T existedData, T addData) {
+    // head부터 특정 노드 찾기
+    public T searchFromHead(T data) {
+        // 1. 리스트에 노드가 하나도 없는지
         if(head == null) {
-            head = new Node<T>(addData);
-            tail = head;
-            return true;
-        } else if(head.data == existedData) {
-            Node<T> newHead = new Node<T>(addData);
-            newHead.next = head;
-            head = newHead;
-            head.next.prev = head;
-            return true;
+            return null;
+        // 2. 리스트에 노드가 하나 이상 있는지
         } else {
             Node<T> node = head;
             while(node != null) {
-                if(node.data == existedData) {
-                    Node<T> nodePrev = node.prev;  
+                if(node.data == data) {
+                    return node.data;
+                } else {
+                    node = node.next;
+                }
+            }
+            return null;
+        }
+    }
+    
+    // 임의 노드 앞에 노드 추가
+    public boolean insertToFront(T searchNode, T data) {
+        // 1. 리스트에 노드가 하나도 없는지
+        if(head == null) {
+            head = new Node<T>(data);
+            tail = head;
+            return true;
+        // 2. 리스트에 노드가 하나 이상 있는지
+        // 2-1. head가 앞의 노드 -> head를 바꿔야 함
+        } else if(head.data == searchNode) {
+            Node<T> newHead = new Node<T>(data);
+            newHead.next = this.head;
+            head = newHead;
+            head.next.prev = head;
+            return true;
+            // 2-2. 일반적인 경우
+        } else {
+            Node<T> node = head;
+            while(node != null) {
+                if(node.data == searchNode) {
+                    Node<T> prevNode = node.prev;
                     
-                    nodePrev.next = new Node<T>(addData);
-                    nodePrev.next.next = node;
+                    prevNode.next = new Node<T>(data);
+                    prevNode.next.next = node;
                     
-                    nodePrev.next.prev = nodePrev;
-                    node.prev = nodePrev.next;
+                    prevNode.next.prev = prevNode;
+                    node.prev = prevNode.next;
                     return true;
+                    
                 } else {
                     node = node.next;
                 }
             }
             return false;
+            
         }
+
     }
     
     public static void main(String[] args) {
-    	DoubleLinkedList<Integer> myLinkedList = new DoubleLinkedList<Integer>();
+        DoubleLinkedList<Integer> MyLinkedList = new DoubleLinkedList<Integer>();
 
-    	myLinkedList.addNode(1);
-    	myLinkedList.addNode(2);
-    	myLinkedList.addNode(3);
-    	myLinkedList.addNode(4);
-    	myLinkedList.addNode(5);
-    	myLinkedList.printAll();
+        MyLinkedList.addNode(1);
+        MyLinkedList.addNode(2);
+        MyLinkedList.addNode(3);
+        MyLinkedList.addNode(4);
+        MyLinkedList.addNode(5);
+        MyLinkedList.printAll();
+        System.out.println("----------------");
 
-    	System.out.println("===================");
-    	myLinkedList.insertToFront(3,2);
-    	myLinkedList.printAll();
+        MyLinkedList.insertToFront(3, 2);
+        MyLinkedList.printAll();
+        System.out.println("----------------");
 
-    	System.out.println("===================");
-    	myLinkedList.insertToFront(5,8);
-    	myLinkedList.printAll();
+        MyLinkedList.insertToFront(6, 2);
+        MyLinkedList.insertToFront(1, 0);
+        MyLinkedList.printAll();
+        System.out.println("----------------");
 
-    	System.out.println("===================");
-    	myLinkedList.addNode(7);
-    	myLinkedList.printAll();
-
-    	System.out.println("===================");
-    	myLinkedList.insertToFront(1,10);
-    	myLinkedList.printAll();
+        MyLinkedList.addNode(6);
+        MyLinkedList.printAll();
     }
-    
+
 }
