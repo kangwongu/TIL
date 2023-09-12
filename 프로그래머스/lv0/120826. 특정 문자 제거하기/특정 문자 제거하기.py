bbc2a010d3900a7da63cf38ea96534ed94f6bb00
kangwongu <59812251+kangwongu@users.noreply.github.com>
2023-09-12 10:30:07 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
@@ -1,8 +1,2 @@
 def solution(my_string, letter):
-    answer = ''
-    for ch in my_string:
-        if letter == ch:
-            continue
-        answer += ch
-
-    return answer
+    return my_string.replace(letter, '')
