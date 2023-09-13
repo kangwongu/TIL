c367ff074378e04eb79758df49fdcbbfc1fd03c9
kangwongu <59812251+kangwongu@users.noreply.github.com>
2023-09-13 10:28:09 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.py
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.py
@@ -1,6 +1,19 @@
 def solution(age):
     answer = ''
     str_age = str(age)
-    answer = str_age.replace("0", "a").replace("1", "b").replace("2", "c").replace("3","d").replace("4", "e").replace("5", "f").replace("6", "g").replace("7", "h").replace("8", "i").replace("9", "j")
-    
+    conv = {
+        '0':'a',
+        '1':'b',
+        '2':'c',
+        '3':'d',
+        '4':'e',
+        '5':'f',
+        '6':'g',
+        '7':'h',
+        '8':'i',
+        '9':'j'
+    }
+
+    answer = ''.join(conv[ch] for ch in str_age)
+
     return answer
