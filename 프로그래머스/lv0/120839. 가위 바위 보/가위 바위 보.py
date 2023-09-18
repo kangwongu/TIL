7ec871652cfd5a65856fd2139613a69993f1c74c
kangwongu <59812251+kangwongu@users.noreply.github.com>
2023-09-18 10:18:08 +0900

    [level 0] Title: 가위 바위 보, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.py
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.py
@@ -1,10 +1,7 @@
 def solution(rsp):
     answer = ''
+    to_win = {'2':'0', '0':'5', '5':'2'}
+
     for ch in rsp:
-        if ch=='2':
-            answer += '0'
-        elif ch=='0':
-            answer += '5'
-        elif ch=='5':
-            answer += '2'
+        answer += to_win[ch]
     return answer
