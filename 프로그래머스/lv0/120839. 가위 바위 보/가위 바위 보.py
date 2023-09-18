7708d3f5acddced9bc7bbcc4aed195bd931efe5a
kangwongu <59812251+kangwongu@users.noreply.github.com>
2023-09-18 10:14:55 +0900

    [level 0] Title: 가위 바위 보, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.py
@@ -0,0 +1,10 @@
+def solution(rsp):
+    answer = ''
+    for ch in rsp:
+        if ch=='2':
+            answer += '0'
+        elif ch=='0':
+            answer += '5'
+        elif ch=='5':
+            answer += '2'
+    return answer
