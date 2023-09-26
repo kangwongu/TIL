94ec0e6ec2f8b457794c644041c2430618372d3b
kangwongu <59812251+kangwongu@users.noreply.github.com>
2023-09-26 09:39:03 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.py
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.py
@@ -1,8 +1,6 @@
 def solution(my_string):
     answer = ''
-    for i in range(len(my_string)):
-        if my_string.find(my_string[i]) == i:
-            print(my_string.find(my_string[i]))
-            answer += my_string[i]
-            
+    for i in my_string:
+        if i not in answer:
+            answer += i
     return answer
