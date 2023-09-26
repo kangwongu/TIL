e3945bb74be1e9e416cd90e35377022f822b8cfa
kangwongu <59812251+kangwongu@users.noreply.github.com>
2023-09-26 09:07:56 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.py
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.py
@@ -1,9 +1,9 @@
 def solution(s):
-    answer = 0
-    s_arr = s.split()
-    for i in range(len(s_arr)):
-        if s_arr[i] == 'Z':
-            answer -= int(s_arr[i-1])
+    stack = []
+    for ch in s.split():
+        if ch != 'Z':
+            stack.append(int(ch))
         else:
-            answer += int(s_arr[i])
-    return answer
+            stack.pop()
+
+    return sum(stack)
