4c4ad4248459285ef95dd4140e2bb9a183d69fd7
kangwongu <59812251+kangwongu@users.noreply.github.com>
2023-10-14 23:53:12 +0900

    [level unrated] Title: 문자열 계산하기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120902. 문자열 계산하기/문자열 계산하기.py
+++ 프로그래머스/unrated/120902. 문자열 계산하기/문자열 계산하기.py
@@ -1,2 +1,11 @@
 def solution(my_string):
-    return eval(my_string)
+    array = my_string.split()
+    answer = int(array[0])
+
+    for i in range(1,len(array),2):
+        if array[i]=='+':
+            answer+=int(array[i+1])
+        else:
+            answer-=int(array[i+1])
+    
+    return answer
